9df842c48f38ec74e2d96ba62867ae99c1a2dfed
hakan <hakan@DESKTOP-7IJG156.local>
2022-11-27 22:12:43 +0300

    16 - Implement Models and add migration

--- HotelListing/HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing/HotelListing.API/Data/HotelListingDbContext.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+namespace HotelListing.API.Data;
 
-namespace HotelListing.API.Data
+public class HotelListingDbContext : DbContext
 {
-    public class HotelListingDbContext : DbContext
-    {
-        public HotelListingDbContext(DbContextOptions options) : base(options)
-        {
-
-        }
-    }
+    public HotelListingDbContext(DbContextOptions options) : base(options) { }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Country> Countries { get; set; }
 }
+
