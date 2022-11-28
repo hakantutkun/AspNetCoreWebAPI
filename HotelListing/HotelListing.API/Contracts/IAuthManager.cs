1caad1fc870f756f671ca96c32d778fe151b04e3
hakan <hakan@DESKTOP-7IJG156.local>
2022-11-29 01:36:26 +0300

    45 - Setup auth controller for register

---
+++ HotelListing/HotelListing.API/Contracts/IAuthManager.cs
@@ -0,0 +1,9 @@
+﻿using HotelListing.API.Models.Users;
+
+namespace HotelListing.API.Contracts
+{
+    public interface IAuthManager
+    {
+        Task<bool> Register(ApiUserDto userDto);
+    }
+}
