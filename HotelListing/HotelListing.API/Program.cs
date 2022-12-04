ed933f750bd1d4e0980cafcd88ad19ac5dba9f59
hakan <hakan@DESKTOP-7IJG156.local>
2022-12-04 22:22:53 +0300

    latest

--- HotelListing/HotelListing.API/Program.cs
+++ HotelListing/HotelListing.API/Program.cs
@@ -133,12 +133,8 @@ builder.Services.AddControllers().AddOData(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
